cc6e30e9519d968c7ac8fced0ee9594b06902d02
Maahi mishra <119564045+MaahiMishra12222768@users.noreply.github.com>
2025-06-23 10:49:49 +0530

    Update geektrust.py

--- python-pip-starter-kit/geektrust.py
+++ python-pip-starter-kit/geektrust.py
@@ -1,37 +1,19 @@
-
 from sys import argv
-
+from src.CommandFactory import factory
 def main():
-    if len (argv)!= 2:
-        raise Exception("File path not entered")
-    file_path = argv[1]
-    with open(file_path ,  "r") as file  :
-        for line in file:
-            arr = line.split()
-            print(arr)
-        if arr[0] == "BALANCE" :
-            pass
-        elif arr[0] == "CHECK_IN" :
-            pass
-        elif arr[0] =="PRINT_SUMMARY":
-            pass
-
-        
-    
-
-    
-
-    
-    """
-    Sample code to read inputs from the file
-
     if len(argv) != 2:
         raise Exception("File path not entered")
     file_path = argv[1]
-    f = open(file_path, 'r')
-    Lines = f.readlines()
-    //Add your code here to process the input commands
-    """
+
+    with open(file_path ,  "r") as file  :
+        for line in file  :
+            arr =   line.split()
+            command = arr[0]
+            args =  arr[1:]
+            handler  = factory.get_handler(command)
+            handler.handle_command(args)
+
+
     
 if __name__ == "__main__":
     main()
