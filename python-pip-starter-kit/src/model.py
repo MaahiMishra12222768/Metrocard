cc17a31dc6314b766abfaa22ea47e27224764b85
Maahi mishra <119564045+MaahiMishra12222768@users.noreply.github.com>
2025-06-23 10:17:48 +0530

    Update model.py

--- python-pip-starter-kit/src/model.py
+++ python-pip-starter-kit/src/model.py
@@ -1,29 +1,47 @@
 from collections import defaultdict
 class MetroCard :
-    def __init__(self , id , balance):
-        self.id = id 
+    def __init__(self , id ,  balance):
+        self.id  =  id
         self.src = None
-        self.balance = balance 
+        self.balance  =  balance
 
-    def add_balance(self , amount):
-        self.balance += amount
 
-    
-class station :
+    def add_balance(self , ammount):
+        self.balance+=ammount
+
+    def update_src(self ,  src):
+        self.src =  src
+
+
+class Station  :
     def __init__(self , name):
-        self.name = name 
-        self.total_amount = 0 
-        self.discount = 0 
-        self.passenger = defaultdict(int)
+        self.name  =  name
+        self.total_ammount = 0
+        self.discount  =  0
+        self.passengerHistory  =  defaultdict(int)
 
 
-    def add_amount(self , x ):
-        self.total_amount += x 
-    
-    def add_discount(self , x ):
-        self.discount += x 
+    def add_ammount(self ,  x):
+        self.total_ammount+=x
 
-    def add_passenger(self , type):
-        self.passenger[type]+=1
-    
+    def add_discount(self , x):
+        self.discount+=x
+
+    def add_passenger(self ,  type):
+        self.passengerHistory[type]+=1
+
+class Fare :
+    rates = {
+        "ADULT": 200,
+        "SENIOR_CITIZEN": 100,
+        "KID": 50
+    }
+
+    @classmethod
+    def get_fare(cls ,  type ,  is_round_trip):
+        base =  cls.rates[type]
+        if is_round_trip :
+            base /=2
+        return  base
+
 
